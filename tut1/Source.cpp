9241c0a65983a72afbed8b77c23414bebba254ff
dineshri <dineshripillay@yahoo.com>
2016-03-22 18:35:44 +0200

    newbranch commit 1-50

--- tut1/Source.cpp
+++ tut1/Source.cpp
@@ -3,27 +3,38 @@ using namespace std;
 
 int main() {
 
-	cout << "Enter a number from 1-20" << endl;
+	cout << "Enter a number from 1-50" << endl;
 	int number;
 	cin >> number;
 
-	if (number < 1 || number > 20){
+	if (number < 1 || number > 50){
 		cout << "invalid number!";
 		system("pause");
 		return 0;
 
 	}
 
-	if (number % 15 == 0)
-		cout << "fizzbuzzwoof";
-     
+	if (number % 3 == 0 && number % 5 == 0 && number % 7 == 0)
+		cout << "fizzbuzzwoof" << endl;
+
+	else if (number % 5 == 0 && number % 7 == 0)
+		cout << "buzzwoof" << endl;
+
+	else if (number % 3 == 0 && number % 7 == 0)
+		cout << "fizzwoof" << endl;
+
+	else if (number % 3 == 0 && number % 5 == 0)
+		cout << "fizzbuzz" << endl;
+
+	else if (number % 7 == 0)
+		cout << "woof" << endl;
+
 
 	else if (number % 5 == 0)
 		cout << "buzz";
 
 	else if (number % 3 == 0)
-		cout << "fizz";
+		cout << "fizz" << endl;
 
-	else if (number % 3 == 0 && number % 5 == 0)
-		cout << "fizzbuzz";
+	system("pause");
 }
